398ea5ae27a60ca9bafa222a70260f54388a15b7
James Hickey <jamesmichaelhickey@gmail.com>
2019-12-08 03:38:54 -0400

    working with composite ui

--- HostApp/Startup.cs
+++ HostApp/Startup.cs
@@ -24,6 +24,7 @@ namespace HostApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,7 @@ namespace HostApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
             });
         }
     }
